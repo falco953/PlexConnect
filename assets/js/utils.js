8ba2193c611fa61d08632c59fb7592a20906abdb
falco953 <you@example.com>
2014-05-15 14:50:18 +0200

    updates

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -135,6 +135,43 @@ function refreshSection(id, refreshKey)
 }
 
 /*
+ * navigation bar - dynamic loading of manu pages
+ */
+function loadMenuPage(event)
+{
+    var id = event.navigationItemId;
+    log("loadItem: "+id);
+    var item = document.getElementById(id);
+    var url = item.getElementByTagName('url').textContent;
+    
+    if (url.indexOf("{{URL(/)}}")!=-1)
+    {
+        url = url + "&PlexConnectUDID=" + atv.device.udid;
+    }
+    
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function()
+    {
+        try
+        {
+            if(req.readyState == 4)
+            {
+                doc = req.responseXML
+                if(event) event.success(doc);
+                else atv.loadXML(doc);
+            }
+        }
+        catch(e)
+        {
+            req.abort();
+        }
+    }
+    req.open('GET', url, true);
+    req.send();
+};
+
+
+/*
  * lookup movie title on tmdb and pass trailer ID to PlexConnect
  */
 function playTrailer(title,year)
@@ -206,7 +243,7 @@ var iOS_atv_loadURL = atv.loadURL;
 atv.loadURL = function(url)
 {
     log("loadURL (override): "+url);
-    if (url.indexOf("{{URL()}}")!=-1)
+    if (url.indexOf("{{URL(/)}}")!=-1)
     {
         url = url + "&PlexConnectUDID=" + atv.device.udid;
         url = url + "&PlexConnectATVName=" + encodeURIComponent(atv.device.displayName);
@@ -225,7 +262,7 @@ var iOS_atv_loadAndSwapURL = atv.loadAndSwapURL;
 atv.loadAndSwapURL = function(url)
 {
     log("loadAndSwapURL (override): "+url);
-    if (url.indexOf("{{URL()}}")!=-1)
+    if (url.indexOf("{{URL(/)}}")!=-1)
     {
         url = url + "&PlexConnectUDID=" + atv.device.udid;
         url = url + "&PlexConnectATVName=" + encodeURIComponent(atv.device.displayName);
